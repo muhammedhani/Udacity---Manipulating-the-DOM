35f890339d7577e9b7191e66823dd49caea4e15a
muhammedhani <mohammedhani.mh71@gmail.com>
2022-06-21 04:51:43 +0200

    adding all basic features

--- js/app.js
+++ js/app.js
@@ -36,10 +36,50 @@
  */
 
 // build the nav
+const navList = document.getElementById('navbar__list');
+let fragment = document.createDocumentFragment();
+for (let i = 1; i <= 4; i++) {
+	let listItem = document.createElement('li');
+	listItem.innerHTML = `<a href="#section${i}" class="menu__link">Section ${i}</a>`;
+	fragment.appendChild(listItem);
+}
+navList.appendChild(fragment);
 
 // Add class 'active' to section when near top of viewport
+const sections = document.getElementsByClassName('section');
+setTimeout(() => {
+	document.addEventListener('scroll', () => {
+		for (let i of sections) {
+			let rect = i.getBoundingClientRect(); // Get the bounds of a section as a dictionary
+			let y = rect['y']; // Get the value of y-coordinate of the section
+			if (y <= 100 && y >= -100) {
+				// check if the section is on the viewport
+				i.classList.add('your-active-class');
+			} else {
+				i.classList.remove('your-active-class');
+			}
+		}
+	});
+}, 0);
 
 // Scroll to anchor ID using scrollTO event
+const anchors = document.getElementsByClassName('menu__link');
+for (let i of anchors) {
+	i.addEventListener('click', function (evt) {
+		evt.preventDefault(); // prevent the default anchor behavior
+		let target = i.getAttribute('href'); // get the targetElement id (the "#" included)
+		target = target.substring(1, target.length + 1); // Get rid of the "#"
+		let targetElement = document.getElementById(target); // select the target section
+		let rect = targetElement.getBoundingClientRect(); // get detect section location relative to the viewport
+		let y = rect['y']; // only "y" is the important
+		targetElement.scrollIntoView({
+			// scroll to section
+			top: y,
+			left: 0,
+			behavior: 'smooth',
+		});
+	});
+}
 
 /**
  * End Main Functions
